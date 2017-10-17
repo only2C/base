ef9272de87c178af0b97c3b4c5adce596f913ebe
chenli <chenliw@yonyou.com>
2017-10-17 16:58:58 +0800

    新增图表分析功能

--- src/containers/bossBill/AddMoney.js
+++ src/containers/bossBill/AddMoney.js
@@ -3,6 +3,7 @@ import {observer} from 'mobx-react';
 import globalStore from '../../stores/GlobalStore';
 import _ from  'lodash';
 import {MonthPicker} from 'ssc-grid';
+import ReactEcharts from 'echarts-for-react';
 import Util from '../../common/utils';
 import {Button,Modal,DropdownButton,MenuItem} from 'react-bootstrap';
 import Nav from '../../containers/bossBill/Nav';
@@ -39,15 +40,32 @@ export default class AddMoney extends React.Component {
                     </div>
                 </div>
                 <div className="stdreimburse-box">
-                    <h3>15日 星期五 收入1000</h3>
-                    <div className="row" style={{"line-height":"35px"}}>
+                    <div className="row">
+                        <div className="col-md-5">
+                            <ReactEcharts
+                                option={this.getOption()}
+                                style={{height: '300px', width: '100%'}}
+                                className='echarts-for-echarts'
+                                theme='my_theme' />
+                        </div>
+                        <div className="col-md-2 b-add-total">
+                            <span>已收款：12345.45 元</span>
+                            <span className="red">欠收款：12345.45 元</span>
+                            <span className="blue">预收款：12345.45 元</span>
+                        </div>
+                    </div>
+
+
+
+                    <h3 className="b-add-h3">15日 星期五 收入1000</h3>
+                    <div className="row b-add-body" style={{"line-height":"35px"}}>
                         <div className="col-md-3">铭迈  2223</div>
                         <div className="col-md-3">铭迈  2223</div>
                         <div className="col-md-3">铭迈  2223</div>
                         <div className="col-md-3">铭迈  2223</div>
                     </div>
-                    <h3>15日 星期五 收入1000</h3>
-                    <div className="row" style={{"line-height":"35px"}}>
+                    <h3 className="b-add-h3">15日 星期五 收入1000</h3>
+                    <div className="row b-add-body" style={{"line-height":"35px"}}>
                         <div className="col-md-3">铭迈  2223</div>
                         <div className="col-md-3">铭迈  2223</div>
                         <div className="col-md-3">铭迈  2223</div>
@@ -66,8 +84,54 @@ export default class AddMoney extends React.Component {
         )
 
     }
+    handleTimeChange = (value, formattedValue)=>{
+        this.setState({
+            timeValue:formattedValue
+        })
+    }
 
 
+    getOption =()=>{
+        let option = {
+            title : {
+                text: '收款一览表',
+                subtext: '',
+                x:'center'
+            },
+            tooltip : {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left',
+                data: ['欠收款','已收款','预收款']
+            },
+            series : [
+                {
+                    name: '状态预览',
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['50%', '60%'],
+                    data:[
+                        {value:310, name:'欠收款'},
+                        {value:335, name:'已收款'},
+                        {value:234, name:'预收款'},
+
+                    ],
+                    color:['red','blue','#cfcfcf'],
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+        return option;
+    }
     //取消
     exit = ()=>{
         window.location.hash= '#/bossBill'
